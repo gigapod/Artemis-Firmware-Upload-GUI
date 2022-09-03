c72cf1794949169655326c5c4166b384cde8a557
Kirk Benell <kirk.benell@sparkfun.com>
2022-09-02 19:46:39 -0600

    console color tweak

--- tools/artemis_firmware_uploader_gui.py
+++ tools/artemis_firmware_uploader_gui.py
@@ -406,7 +406,7 @@ class MainWindow(QMainWindow):
 
         # Messages Window
         self.messages = QPlainTextEdit()
-        self.messages.setStyleSheet("QPlainTextEdit { color: #ED820E;}")
+        self.messages.setStyleSheet("QPlainTextEdit { color: #C0C0C0;}")
         # Attempting to reduce window size
         #self.messages.setMinimumSize(1, 2)
         #self.messages.resize(1, 2)
